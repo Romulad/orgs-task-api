fc3e58701afe153881d884d3a3c4440e84f2a61c
Romuald Oluwatobi <romualdnoualinon@gmail.com>
2025-05-19 14:03:59 +0100

    update: chage user model to only includ necessary field

--- user/models.py
+++ user/models.py
@@ -1,12 +1,9 @@
-import uuid
-
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import EmailValidator
 
 from .user_manager import CustomUserManager
-from organization.models import Organization
 from app_lib.models import AbstractBaseModel
 
 class AppUser(AbstractBaseModel, AbstractBaseUser, PermissionsMixin):
@@ -36,25 +33,6 @@ class AppUser(AbstractBaseModel, AbstractBaseUser, PermissionsMixin):
             "Designates whether this user should be treated as active. "
             "Unselect this instead of deleting accounts."
         ),
-    )
-    is_owner = models.BooleanField(
-        _("is owner"),
-        default=False,
-        help_text=_(
-            "Designates whether this user is an organization owner."
-            "A owner by default has a full access over his ressources"
-        ),
-    )
-    is_manager = models.BooleanField(
-        _("is manager"),
-        default=False,
-        help_text=_(
-            "Designates whether this user is an organization manager."
-            "A manager by default has a read/update access over ressources"
-        ),
-    )
-    orgs = models.ManyToManyField(
-        Organization, verbose_name=_('organizations'), blank=True,
     )
     
     objects = CustomUserManager()
