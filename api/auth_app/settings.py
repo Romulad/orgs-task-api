3ecae100eb64318700020b056521033a785384c7
Romuald Oluwatobi <romualdnoualinon@gmail.com>
2025-07-05 11:20:04 +0100

    feat: make documentation separate from the api during deployement

--- api/auth_app/settings.py
+++ api/auth_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.localhost']
 
 
 # Application definition
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
     'drf_spectacular',
     'django_filters',
     'corsheaders',
@@ -52,6 +51,9 @@ INSTALLED_APPS = [
     'tags',
     'perms'
 ]
+if DEBUG:
+    INSTALLED_APPS.append('rest_framework')
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
--- api/auth_app/urls.py
+++ api/auth_app/urls.py
@@ -9,19 +9,24 @@ from drf_spectacular.views import (
 
 
 urlpatterns = [
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name="swagger"),
     path('auth/', include('auth_user.urls')),
     path('', include('user.urls')),
     path('perms/', include('perms.urls')),
     path('', include('organization.urls')),
     path('', include('tags.urls')),
     path('', include('tasks.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
-    path('redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
+
 if settings.DEBUG:
-    urlpatterns.append(
-        path('admin/', admin.site.urls)
+    extra = [
+        path('admin/', admin.site.urls),
+        path('api-auth/', include('rest_framework.urls')),
+        path("api/schema", SpectacularAPIView.as_view(), name="schema"),
+        path('redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    ]
+    urlpatterns.extend(extra)
+    urlpatterns.insert(
+        0,
+        path('', SpectacularSwaggerView.as_view(url_name='schema'), name="swagger")
     )
